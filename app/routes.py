787553d4c7e744c3e08f6d3366dc3532e8b3ec26
jeffreyjose07 <jeffreyjose.k@gmail.com>
2020-07-14 14:24:55 +0530

    Display users in loop

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Jose'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - My web site</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    posts = [
+        {
+            'author': {'username': 'Jose'},
+            'body': 'Beautiful day!'
+        },
+        {
+            'author': {'username': 'Mini'},
+            'body': 'Its a rainy day!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
